a171f38d59317c0448cd62601656b64ec1899109
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-09 14:33:59 +0200

    Add map initialization with OpenStreetMap layer
    Initialize map with OpenStreetMap tiles.

--- js/gps.js
+++ js/gps.js
@@ -103,6 +103,15 @@ function getLocation(){
         }
 
     );
+const map = L.map('map').setView([52.3676, 4.9041], 12);
+
+L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: 'OpenStreetMap'
+    }
+).addTo(map);
+
 
 }
 
