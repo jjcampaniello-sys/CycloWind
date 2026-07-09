d4d21220a0917610b5149dd98fdcafd87b30ad26
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-09 08:51:44 +0200

    Initialize map with OpenStreetMap and marker

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// Création de la carte
+const map = L.map('map').setView([52.3676, 4.9041], 12);
 
+// Fond de carte OpenStreetMap
+L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: 'OpenStreetMap'
+    }
+).addTo(map);
+
+// Position test
+L.marker([52.3676, 4.9041])
+    .addTo(map)
+    .bindPopup("CycloWind")
+    .openPopup();
