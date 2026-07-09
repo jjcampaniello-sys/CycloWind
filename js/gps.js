54923443a536f373bfb1a162485a4cf1ff4503cf
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-09 14:31:57 +0200

    Implement GPS location retrieval and display
    Added functionality to get the user's current GPS location and display it on the map with markers for the user's position and bike direction.

--- js/gps.js
+++ js/gps.js
@@ -1,1 +1,108 @@
+// Variables globales
+let currentWindDirection = 0;
+let currentWindSpeed = 0;
+
+let marker;
+let bikeArrow;
+let windControl;
+let windLegend;
+let routeLine;
+
+
+
+// GPS
+function getLocation(){
+
+
+    navigator.geolocation.getCurrentPosition(
+
+        function(position){
+
+
+            const lat =
+            position.coords.latitude;
+
+
+            const lon =
+            position.coords.longitude;
+
+
+            const rideDirection =
+            position.coords.heading || 0;
+
+
+
+            if(marker){
+                map.removeLayer(marker);
+            }
+
+
+
+            marker =
+            L.marker([lat,lon])
+            .addTo(map)
+            .bindPopup("Vous êtes ici");
+
+
+
+            if(bikeArrow){
+                map.removeLayer(bikeArrow);
+            }
+
+
+
+            const icon =
+            L.divIcon({
+
+                html:
+                `
+                <div style="
+                transform:rotate(${rideDirection}deg);
+                font-size:50px;
+                color:blue;">
+                ➤
+                </div>
+                `
+
+            });
+
+
+
+            bikeArrow =
+            L.marker(
+                [lat,lon],
+                {icon:icon}
+            )
+            .addTo(map);
+
+
+
+            map.setView(
+                [lat,lon],
+                19
+            );
+
+
+
+            getWind(
+                lat,
+                lon,
+                rideDirection
+            );
+
+        },
+
+
+        function(){
+            alert("GPS impossible");
+        },
+
+
+        {
+            enableHighAccuracy:true
+        }
+
+    );
+
+}
 
