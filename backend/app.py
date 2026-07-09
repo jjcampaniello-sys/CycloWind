6207493a5721d38df46884d6f481890dd5ed06c9
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-09 08:48:13 +0200

    Delete backend directory

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routing import best_route
-
-app = FastAPI()
-
-@app.get("/route")
-def get_route(wind_dir: float = 0, wind_speed: float = 10):
-    path = best_route(wind_dir, wind_speed)
-    return {"route": path}
