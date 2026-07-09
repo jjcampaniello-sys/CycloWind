50185a6d3c0e555ee7e84ce5ba1c8afeb173c31c
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-09 14:33:37 +0200

    Refactor map initialization and add variables

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
-const map = L.map('map').setView([52.3676, 4.9041], 12);
+const map = L.map('map')
+    .setView([52.3676,4.9041],12);
 
 L.tileLayer(
-    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-        attribution: 'OpenStreetMap'
-    }
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+ attribution:'OpenStreetMap'
+}
 ).addTo(map);
+
+
+let marker;
+let bikeArrow;
+let windControl;
+let windLegend;
+
+let currentWindDirection = 0;
+let currentWindSpeed = 0;
+let routeLine;
+
+
+// Démarrage
 
 
