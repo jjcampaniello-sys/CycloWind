b29bac2da9ecdf5ea6fd24a1ffa4c7a55c84d1da
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-10 09:53:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,31 +2,6 @@ const map = L.map('map')
     .setView([52.3676,4.9041],12);
 
 
-if(navigator.geolocation){
-
-    navigator.geolocation.getCurrentPosition(
-
-        function(position){
-
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-
-            map.setView(
-                [lat,lon],
-                15
-            );
-
-        },
-
-        function(){
-
-            console.log("GPS indisponible");
-
-        }
-
-    );
-
-}
 
 L.tileLayer(
 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
