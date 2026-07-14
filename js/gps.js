923f05fd2aaeac5c9bbb96778904f0554a43e270
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-14 06:19:50 +0200

    Update gps.js

--- js/gps.js
+++ js/gps.js
@@ -146,93 +146,96 @@ function moveMarkerOnRoute(point) {
     window.userMarker.setLatLng([point.lat, point.lng]);
 }
 //-----------------
-        function(position){
+    function updateUserMarker(lat, lon){
 
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
+    if(marker){
+        marker.setLatLng([lat,lon]);
+    }
+    else{
+        marker = L.marker([lat,lon])
+        .addTo(map)
+        .bindPopup("Vous êtes ici");
+    }
 
-         const rideDirection = currentHeading;
+}   
+//-------------------
+// function(position){
 
+        ///    const lat = position.coords.latitude;
+       //     const lon = position.coords.longitude;
 
-            if(marker){
-                map.removeLayer(marker);
-            }
-
-
-            marker =
-            L.marker([lat,lon])
-            .addTo(map)
-            .bindPopup("Vous êtes ici")
-            .openPopup();
-
-
-
-            if(bikeArrow){
-                map.removeLayer(bikeArrow);
-            }
+      //   const rideDirection = currentHeading;
 
 
-            const icon =
-L.divIcon({
+           // if(marker){
+          //      map.removeLayer(marker);
+          //  }
 
-    className: "bike-icon",
 
-    html:
+          //  marker =
+          //  L.marker([lat,lon])
+           // .addTo(map)
+           // .bindPopup("Vous êtes ici")
+          //  .openPopup();
+
+          //  if(bikeArrow){
+         //       map.removeLayer(bikeArrow);
+        //    }
+
+          //  const icon =
+//L.divIcon({
+
+  //  className: "bike-icon",
+
+  //  html:
     `
-    <div style="
-    transform:rotate(${rideDirection}deg);
-    font-size:28px;
-    color:blue;">
-    ➤
-    </div>
+   // <div style="
+  //  transform:rotate(${rideDirection}deg);
+//    font-size:28px;
+//    color:blue;">
+ //   ➤
+ //   </div>
     `,
 
-    iconSize:[35,35],
-    iconAnchor:[17,17]
+   // iconSize:[35,35],
+   // iconAnchor:[17,17]
 
-});
+//});
 
+    //        bikeArrow =
+        //    L.marker(
+        //        [lat,lon],
+         //       {icon:icon}
+         //   )
+         //   .addTo(map);
 
+          //  map.setView(
+         //       [lat,lon],
+         //       19
+         //   );
 
-            bikeArrow =
-            L.marker(
-                [lat,lon],
-                {icon:icon}
-            )
-            .addTo(map);
-
-
-
-            map.setView(
-                [lat,lon],
-                19
-            );
-
-
-            getWind(
-                lat,
-                lon,
-                rideDirection
-            );
+         //   getWind(
+          //      lat,
+         //       lon,
+          //      rideDirection
+          //  );
 
         },
 
+       // function(error){
 
-        function(error){
+       //     alert(
+        //    "GPS impossible : " + error.message
+       //     );
 
-            alert(
-            "GPS impossible : " + error.message
-            );
+      //  },
 
-        },
+     //   {
+     ///       enableHighAccuracy:true,
+     //       timeout:10000
+     //   }
 
+   // );
+  //  startCompass();
 
-        {
-            enableHighAccuracy:true,
-            timeout:10000
-        }
-
-    );
-    startCompass();
-
-}
+//}
