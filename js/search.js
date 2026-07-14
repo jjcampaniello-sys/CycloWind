bd0aad124025a71368fbcfa28757b5dd84e48370
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-14 17:34:25 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -8,15 +8,39 @@ function searchDestination(){
 
     if(query.length < 3) return;
 
-    console.log("Recherche lancée :", query);
-
     fetch(`https://photon.komoot.io/api/?q=${encodeURIComponent(query)}&limit=5&lang=fr`)
         .then(res => res.json())
         .then(data => {
-            console.log("Résultats :", data);
-            alert("Résultats reçus !");
-        })
-        .catch(err => {
-            console.error("Erreur fetch :", err);
+
+            const container = document.getElementById("suggestions");
+            container.innerHTML = "";
+
+            data.features.forEach(place => {
+
+                const name = place.properties.name || "Lieu";
+                const city = place.properties.city || "";
+                const full = name + " " + city;
+
+                const div = document.createElement("div");
+
+                div.innerHTML = full;
+
+                div.style.padding = "10px";
+                div.style.cursor = "pointer";
+
+                div.onclick = function(){
+
+                    // 🔥 STOCKER DESTINATION
+                    window.destination = place.geometry.coordinates;
+
+                    document.getElementById("destination").value = full;
+
+                    container.innerHTML = "";
+
+                    console.log("Destination choisie :", window.destination);
+                };
+
+                container.appendChild(div);
+            });
         });
 }
