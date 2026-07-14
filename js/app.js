bccc7c78a51f4230f4a64014c158bb8c96158d2b
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-14 10:51:04 +0200

    Add startCompass call on window load event

--- js/app.js
+++ js/app.js
@@ -47,10 +47,11 @@ function clearRoute(){
 }
 
 //-------------------------------------
-''window.addEventListener("load", function(){
-''  startGPS();
-''alert("Lancement GPS");
-''});
+window.addEventListener("load", function(){
+  startGPS();
+alert("Lancement GPS");
+      startCompass();
+});
 //----------------------------------------
 
 // Démarrage
