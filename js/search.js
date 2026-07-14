079ad89f8b85432d142a6e5ca33855b1c48959b0
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-14 17:22:30 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,4 +1,5 @@
 alert("search.js chargé");
+
 window.destination = null;
 
 function searchDestination(){
@@ -7,48 +8,15 @@ function searchDestination(){
 
     if(query.length < 3) return;
 
-    console.log("Recherche lancée");
+    console.log("Recherche lancée :", query);
 
-    fetch(`https://photon.komoot.io/api/?q=${query}`)
+    fetch(`https://photon.komoot.io/api/?q=${encodeURIComponent(query)}&limit=5&lang=fr`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            console.log("Résultats :", data);
             alert("Résultats reçus !");
+        })
+        .catch(err => {
+            console.error("Erreur fetch :", err);
         });
-}
-//const apiKey = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImU5N2JkNDJjYTM5MzRjYTFhODQ1MTE2YjViNmQ2ZGJjIiwiaCI6Im11cm11cjY0In0=";
-    const url = `https://photon.komoot.io/api/?q=${encodeURIComponent(cleanedQuery)}&limit=5&lang=fr`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    box.innerHTML = "";
-
-    data.features.forEach((place) => {
-        const item = document.createElement("div");
-        item.className = "suggestion";
-
-        item.innerHTML =
-            (place.properties.housenumber || "") +
-            " " +
-            (place.properties.street || place.properties.name || "") +
-            "<br>" +
-            (place.properties.city || "") +
-            " " +
-            (place.properties.country || "");
-
-        item.onclick = function () {
-            window.destination = {
-                lat: place.geometry.coordinates[1],
-                lon: place.geometry.coordinates[0]
-            };
-
-            document.getElementById("destination").value = item.innerText;
-            box.innerHTML = "";
-
-            console.log("Destination choisie :", window.destination);
-        };
-
-        box.appendChild(item);
-    });
 }
