6d56fedbc575fe34131e4695a33ae91a260a5d79
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-14 08:09:29 +0200

    Remove updateBikeArrowPosition function
    Removed the updateBikeArrowPosition function that manages the bike arrow's position and rotation.

--- js/gps.js
+++ js/gps.js
@@ -167,66 +167,6 @@ function onPositionUpdate(position){
 // Création / déplacement flèche
 // ----------------------------
 
-function updateBikeArrowPosition(lat,lon){
-
-
-    if(!bikeArrow){
-
-
-        bikeArrow =
-        L.marker(
-
-            [lat,lon],
-
-            {
-
-                icon:
-                L.divIcon({
-
-                    className:"bike-icon",
-
-                    html:
-
-                    `
-                    <div style="
-                    transform:rotate(${currentHeading}deg);
-                    font-size:32px;
-                    color:blue;">
-                    ➤
-                    </div>
-                    `,
-
-
-                    iconSize:[40,40],
-
-                    iconAnchor:[20,20]
-
-                })
-
-            }
-
-        )
-        .addTo(map);
-
-
-    }
-    else{
-
-
-        bikeArrow.setLatLng(
-
-            [lat,lon]
-
-        );
-
-
-        updateBikeArrow();
-
-    }
-
-
-}
-
 
 
 // ----------------------------
