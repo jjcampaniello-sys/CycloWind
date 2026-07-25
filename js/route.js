5710acf66f6baccd02c5b13bf98532d992153506
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-25 12:28:44 +0200

    Update route.js

--- js/route.js
+++ js/route.js
@@ -14,7 +14,8 @@ function getSegmentDirection(p1, p2){
 
 async function getAlternativeRoute(start, endLat, endLon) {
     const apiKey = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjliNTU2YzljMDI0YTA1MTlkMjU5YzdkZDM3MzY0YzQzNGIyN2VjYzZhZWQ3YzVkMzk5NmNjNTM4IiwiaCI6Im11cm11cjY0In0="
-    const url = "https://api.openrouteservice.org/v2/directions/cycling-regular/geojson";
+    const url = "https://api.heigit.org/ors/v2/directions/cycling-regular/geojson";
+
   
     const body = {
         coordinates: [
